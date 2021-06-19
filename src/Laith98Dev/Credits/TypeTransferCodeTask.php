43b2204588837088912ce6e2b7802c56ce2fa1a6
Laith Youtuber <49840784+Laith98Dev@users.noreply.github.com>
2021-06-19 13:44:42 -0700

    v1.0.0

---
+++ src/Laith98Dev/Credits/TypeTransferCodeTask.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Laith98Dev\Credits;
+
+/*  
+ *  A plugin for PocketMine-MP.
+ *  
+ *	 _           _ _   _    ___   ___  _____             
+ *	| |         (_) | | |  / _ \ / _ \|  __ \            
+ *	| |     __ _ _| |_| |_| (_) | (_) | |  | | _____   __
+ *	| |    / _` | | __| '_ \__, |> _ <| |  | |/ _ \ \ / /
+ *	| |___| (_| | | |_| | | |/ /| (_) | |__| |  __/\ V / 
+ *	|______\__,_|_|\__|_| |_/_/  \___/|_____/ \___| \_/  
+ *	
+ *  Copyright (C) 2021 Laith98Dev
+ *  
+ *	Youtube: Laith Youtuber
+ *	Discord: Laith98Dev#0695
+ *	Gihhub: Laith98Dev
+ *	Email: help@laithdev.tk
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 	
+ */
+
+use pocketmine\scheduler\Task;
+use pocketmine\Player;
+
+class TypeTransferCodeTask extends Task {
+	
+	/** @var Main */
+	private $plugin;
+	
+	/** @var Player */
+	public $player;
+	
+	/** @var string */
+	public $to;
+	
+	/** @var int */
+	public $count;
+	
+	/** @var string */
+	public $reason;
+	
+	/** @var string */
+	public $code;
+	
+	public function __construct(Main $plugin, Player $player, string $to, int $count, string $reason, string $code){
+		$this->plugin = $plugin;
+		$this->player = $player;
+		$this->to = $to;
+		$this->count = $count;
+		$this->reason = $reason;
+		$this->code = $code;
+	}
+	
+	public function onRun(int $tick){
+		$plugin = $this->plugin;
+		$player = $this->player;
+		$to = $this->to;
+		$count = $this->count;
+		$reason = $this->reason;
+		
+		if(isset($plugin->acceptTransfer[$player->getName()])){
+			unset($plugin->acceptTransfer[$player->getName()]);
+			$player->sendMessage("transfer cancelled, time ended");
+		} else {
+			$this->plugin->transferCredits($sender, $to, $count, $reason);
+		}
+		
+		$this->getHandler()->cancel();
+	}
+}
